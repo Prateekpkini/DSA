e5d744466c13e4c1b2b349e233e5da97ad686e81
Prateekpkini <Prateekpkini2005@gmail.com>
2025-11-26 10:13:34 +0530

    Logical Operations

---
+++ logical_operations.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=12,b=0;
+    //Basic Logical Operations
+    //And && Or || Not !
+    printf("%d\n", a&&b);
+    printf("%d\n", a||b);
+    printf("%d\n", !a);
+    return 0;
+
+}
