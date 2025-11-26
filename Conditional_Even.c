c54db119193c9076857f89e577b6b0e4d7ffe55b
Prateekpkini <Prateekpkini2005@gmail.com>
2025-11-26 10:56:58 +0530

    Even or odd using if else

---
+++ Conditional_Even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    //Conditional
+    //if, if-else, nested if, else-if ladder
+    printf("Enter the number:");
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("a is even\n");
+    }else{
+        printf("a is odd\n");
+    }
+    return 0;
+
+}
