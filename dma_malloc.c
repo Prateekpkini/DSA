610faf0600bc13554a713a58425f6fa241903f6e
Prateekpkini <Prateekpkini2005@gmail.com>
2025-11-28 12:08:03 +0530

    DMA using Malloc Function

---
+++ dma_malloc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int *ptr;
+int main()
+{
+    ptr=(int*)malloc(5*sizeof(int));
+    printf("Enter the value of element:");
+    for(int i=0;i<4;i++){
+        scanf("%d",&ptr[i]);   
+    }
+    for(int i=0;i<4;i++){
+        printf(" %d ",ptr[i]);
+    }
+    free(ptr);
+    return 0;     
+}
