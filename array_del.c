098750791e76f49c847aae0211b399e68ae437aa
Prateekpkini <Prateekpkini2005@gmail.com>
2025-11-28 15:47:41 +0530

    Array deletion at a given position

---
+++ array_del.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int arr[7]={10,20,30,40,50},n=5;
+    printf("Array before deletion:\n");
+    for(int i=0;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
+    printf("\n");
+    printf("Enter the position to delete element:");
+    int pos;
+    scanf("%d",&pos);
+    //deletion at given position
+    for(int i=pos-1;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    n=n-1;      
+    printf("Array after deletion at the given position:\n");
+    for(int i=0;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
