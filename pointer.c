79c55ef7fe17d86d0d193669dc5e1ba4e65292b3
Prateekpkini <Prateekpkini2005@gmail.com>
2025-11-28 09:53:08 +0530

    Three Level Pointer

--- pointer.c
+++ pointer.c
@@ -3,9 +3,14 @@ int main(){
     int a=19;
     int *p=&a;
     int* *n=&p;
+    int** *m=&n;
+    int** *i=&p;
     printf("%p\n",p);
     printf("%d\n",*p);
     printf("%d\n",*(*n));
     printf("%p\n",n);
+    printf("%d\n",*(*(*m)));
+    printf("%p\n",m);
+    printf("%p\n",i);
     return 0;
 }
