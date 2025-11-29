33616316e3ecb813d1fb0788e5361276cf80838a
Prateekpkini <Prateekpkini2005@gmail.com>
2025-11-29 10:05:26 +0530

    Bubble Sort

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 void insertionsort(int arr[],int n){
-    //i for sorted part and j for unsorted part
-    for(int i=1;i<n;i++){
-        int temp=arr[i];
-        int j=i-1;
-        while(j>=0 && arr[j]>temp){
-            arr[j+1]=arr[j];
-            j=j-1;
+    int i,j,temp;
+    for(i=0;i<n;i++){
+        for(j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
         }
-        arr[j+1]=temp;
+   
     }
 }
 
